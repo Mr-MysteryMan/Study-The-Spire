9a94f0a5afce9214a4f0aa9df0ee569b758d2367
link <2459469072@qq.com>
2025-04-12 17:53:21 +0800

    [feat]: 实现房间信息的简单初始化

--- Assets/Scripts/Rooms/MonoBehaviour/Room.cs
+++ Assets/Scripts/Rooms/MonoBehaviour/Room.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    //记录房间在地图上的位置
+    public int col;
+
+    public int row;
+
     private SpriteRenderer spriteRenderer;
 
     public RoomDataSO roomData;
 
+    public RoomState roomState;
+
+    private void Start()
+    {
+        SetupRoom(1, 1, roomData);
+    }
     private void Awake() {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
@@ -13,5 +24,16 @@ public class Room : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("点击了房间：" + roomData.roomType);
+    }
+
+//外部创建房间时进行调用，填充房间的位置/属性
+
+    public void SetupRoom(int col, int row, RoomDataSO roomData)
+    {
+        this.col = col;
+        this.row = row;
+        this.roomData = roomData;
+        Debug.Log("正在设置房间：(" + col + ", "+ row +") "+roomData.roomType);
+        spriteRenderer.sprite = roomData.roomIcon;
     }
 }
--- Assets/Scripts/Utilities/Enums.cs
+++ Assets/Scripts/Utilities/Enums.cs
@@ -1,4 +1,4 @@
-public enum RoomType
+public enum RoomType //房间类型
 {
     MinorEnemy, //普通敌人
     EliteEnemy, //精英敌人
@@ -6,4 +6,11 @@ public enum RoomType
     Treasure, //宝箱
     RestRoom, //休整
     BossRoom
+}
+
+public enum RoomState //房间状态
+{
+    Locked, //暂时无法到达
+    Visited, //已经经过的
+    Attainable //可以前往的
 }
