8140941c8997d8fce4b3f162885e1b6ac54bf76b
Kina <3388310472@qq.com>
2025-04-27 10:23:02 +0800

    添加了继续游戏

---
+++ Assets/Scripts/UI/GameOverPanel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameOverPanel : MonoBehaviour
+{
+    public Button backToMenuButton;
+    public ObjectEventSO loadMenuEvent;
+
+    private void OnEnable()
+    {
+        GetComponent<UIDocument>().rootVisualElement.Q<Button>("BackToMenuButton").clicked += BackToMenu;
+    }
+
+    private void BackToMenu()
+    {
+        loadMenuEvent.RaiseEvent(null, this);
+    }
+}
--- Assets/Scripts/UI/Settings/MenuPanel.cs
+++ Assets/Scripts/UI/Settings/MenuPanel.cs
@@ -4,18 +4,20 @@ using UnityEngine.UIElements;
 public class MenuPanel : MonoBehaviour
 {
     private VisualElement rootElement;
-    private Button newGameButton, quitButton;
+    private Button newGameButton, quitButton,continueButton;
 
     public ObjectEventSO newGameEvent;
+    public ObjectEventSO continueEvent;
 
     private void OnEnable()
     {
         rootElement = GetComponent<UIDocument>().rootVisualElement;
         newGameButton = rootElement.Q<Button>("NewGameButton");
         quitButton = rootElement.Q<Button>("QuitButton");
-
+        continueButton = rootElement.Q<Button>("ContinueGameButton");
         newGameButton.clicked += OnNewGameButtonClicked;
         quitButton.clicked += OnQuitButtonClicked;
+        continueButton.clicked += onContinueButtonClicked;
     }
 
     private void OnQuitButtonClicked()
@@ -27,5 +29,10 @@ public class MenuPanel : MonoBehaviour
     private void OnNewGameButtonClicked()
     {
         newGameEvent.RaiseEvent(null, this);
+    }
+
+    private void onContinueButtonClicked()
+    {
+        continueEvent.RaiseEvent(null, this);
     }
 }
