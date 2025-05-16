817382df3ab77a9615d30e0c57feb4f50c458a4d
link <2459469072@qq.com>
2025-05-16 15:27:10 +0800

    [feat]:添加简单做题内容

--- Assets/Scripts/Rooms/MonoBehaviour/RestRoom.cs
+++ Assets/Scripts/Rooms/MonoBehaviour/RestRoom.cs
@@ -6,12 +6,34 @@ public class RestRoom : MonoBehaviour
 {
     private CardManager cardManager;
     public ObjectEventSO loadMapEvent;
+    public GameObject questionPrefab;
 
-    private void OnMouseDown()
+    private void HandleCorrectAnswer()
     {
-        cardManager = CardManager.Instance;
+        Debug.Log($"回答正确！");
         cardManager.AddHealth(100);
         //返回地图
         loadMapEvent.RaiseEvent(null, this);
+
+    }
+
+    private void HandleWrongAnswer()
+    {
+        Debug.Log($"回答错误！");
+        cardManager.AddHealth(-50);
+        //返回地图
+        loadMapEvent.RaiseEvent(null, this);
+    }
+
+    private void OnMouseDown()
+    {
+        var question = Instantiate(questionPrefab);
+        QuestionManager questionManager = question.GetComponent<QuestionManager>();
+        questionManager.init(
+            () => HandleCorrectAnswer(),  // 正确答案回调
+            () => HandleWrongAnswer()    // 错误答案回调
+        );
+        cardManager = CardManager.Instance;
+     
     }
 }
