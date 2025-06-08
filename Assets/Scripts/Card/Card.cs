bfa2a65e3aaf16767c5e362a382084f6a963f5a7
link <2459469072@qq.com>
2025-06-08 19:38:25 +0800

    [feat]:修改部分UI和休息房间功能

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -102,10 +102,7 @@ public class Card : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
     public void UpdateCardUI() {
         // 按照CardCategory加载背景spirit
         this.background.sprite = cardData.CardCategory switch {
-            CardCategory.Attack => Resources.Load<Sprite>("CardUI/Attack"),
-            CardCategory.Skill => Resources.Load<Sprite>("CardUI/Skill"),
-            CardCategory.Status => Resources.Load<Sprite>("CardUI/Status"),
-            _ => Resources.Load<Sprite>("CardUI/None"),
+            _ => Resources.Load<Sprite>("CardUI/CardBackGround"),
         };
         this.image.sprite = cardData.Sprite; // 设置卡片图片
         this.cardCostText.text = cardData.Cost.ToString();// 加载卡片费用
--- Assets/Scripts/Rooms/MonoBehaviour/RestRoomShowHP.cs
+++ Assets/Scripts/Rooms/MonoBehaviour/RestRoomShowHP.cs
@@ -11,17 +11,26 @@ public class RestRoomShowHP : MonoBehaviour
     private CardManager cardManager;
     [SerializeField] private TMP_Text curHpText;
     [SerializeField] private TMP_Text maxHpText;
-    private Character character;
+    private SpriteRenderer sprite;
+    public Sprite[] healthSprites;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // ��Character����
     public void OnEnable()
     {
         cardManager = CardManager.Instance;
-        Debug.Log("TEXT:");
-        Debug.Log(maxHpText.text);
-        Debug.Log(curHpText.text);
-        maxHpText.text = cardManager.MaxHealth.ToString();
-        curHpText.text = cardManager.Health.ToString();
+        //Debug.Log("TEXT:");
+        //Debug.Log(maxHpText.text);
+        //Debug.Log(curHpText.text);
+        //maxHpText.text = cardManager.MaxHealth.ToString();
+        //curHpText.text = cardManager.Health.ToString();
+        float healthPercentage = (float)cardManager.Health / cardManager.MaxHealth;
+
+        int totalSprites = healthSprites.Length;
+        int spriteIndex = Mathf.FloorToInt(healthPercentage * totalSprites);
+
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sprite = healthSprites[spriteIndex];
+
     }
 }
